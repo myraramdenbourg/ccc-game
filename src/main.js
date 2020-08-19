bb77686ad0b03b5f507edf6ab42f42a98f2154ba
myra <myraramdenbourg@gmail.com>
2020-08-19 16:12:25 -0500

    start with music off

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ class Main extends React.Component {
         super(props);
         this.state = {
             currentLevel: Levels.level1,
-            isPlaying: true,
+            isPlaying: false,
         };
     }
 
